bf5f89e01edb817c3877ead10e226a998c944916
Dovid Krongold <dovidkrongold@Dovids-MacBook-Pro.local>
2021-01-03 15:18:36 +0200

    Add forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,8 @@
 const request = require("request");
 
+
+
+
 const forecast = (latitude, longitude, callback) => {
   const url =
     "http://api.weatherstack.com/current?access_key=8ffe3ddafec45e8a4d54e6751ddb0d57&query=" +
@@ -19,7 +22,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]} and currently ${body.current.temperature} degrees out.  It feels like ${body.current.feelslike} degrees.`
+        `${body.current.weather_descriptions[0]} and currently ${body.current.temperature} degrees out.  It feels like ${body.current.feelslike} degrees.  The humidity is ${body.current.humidity}%.`
       );
     }
   });
